42fcc8b158f9d85c32fcb4c271e55288687a8e93
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-05-04 18:51:37 +0200

    Changed cmap to gray in displaying results

--- src/eval.py
+++ src/eval.py
@@ -21,26 +21,26 @@ def plot_adversarial_example(x, y):
     y_adversarial_pred = y_adversarial_pred_onehot[parameters.adversarial_class]
 
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(10, 5))
-    ax0.imshow(np.squeeze(x))
+    ax0.imshow(np.squeeze(x), cmap='gray')
     ax0.set_title(F"Original, {labels[np.argmax(y)]} = {y_pred:.2f}")
-    ax1.imshow(np.squeeze(adversarial))
+    ax1.imshow(np.squeeze(adversarial), cmap='gray')
     ax1.set_title(F"Adversarial, {labels[parameters.adversarial_class]} = {y_adversarial_pred:.2f}")
 
     fig.tight_layout()
     fig.show()
 
 
-samples = 10
+samples = 20
 dataset = ReducedDataset(FashionMnistDataset(), samples, 0, samples)
 model_provider = TrainedModelProvider()
 model = model_provider.get_model()
 
-parameters = GAParameters(generations=40,
+parameters = GAParameters(generations=100,
                           population_size=30,
                           mutation_probability=0.7,
                           mutation_num_genes=4,
                           crossover_probability=0.6,
-                          perturbation_importance=0.1)
+                          perturbation_importance=0.01)
 
 experiment = Experiment(parameters, model_provider, dataset)
 
